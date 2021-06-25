0e2aa4b1f449f9734079307cc6d804af4661a792
AJeane <ajeane1924@gmail.com>
2021-06-24 23:26:53 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celcius = TempConverter.FahrenheitToCelsius(68); // 20
+            var fahrenheit = TempConverter.CelciusToFahrenheit(20); // 68
+
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"celcius {celcius}");
+            Console.WriteLine($"fahhrenheit {fahrenheit}");
+            
+        }
+    }
+}
